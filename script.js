ff2f94e90fe10a1ef4663689aa17f7836c6770f5
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-02 00:27:53 +0530

    country data api

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
+const countriesContainer = document.querySelector(".countries");
 
-const request = new XMLHttpRequest();
+const getCountryData = function (country) {
+	const request = new XMLHttpRequest();
+	request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+	request.send();
+
+	request.addEventListener("load", function () {
+		const [data] = JSON.parse(this.responseText);
+		console.log(data);
+
+		const html = `<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 cla ss="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages}</p>
+    <p class="country__row"><span>💰</span>${data.currencies}</p>
+  </div>
+</article>`;
+
+		countriesContainer.insertAdjacentHTML("afterbegin", html);
+		countriesContainer.style.opacity = 1;
+	});
+};
+
+getCountryData("bharat");
+getCountryData("usa");
+getCountryData("portugal");
