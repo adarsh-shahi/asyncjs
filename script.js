e47defaabfc263c111a86f8399c000f7f5eef602
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-02 19:21:43 +0530

    callback hell ;)

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const countriesContainer = document.querySelector(".countries");
 
-const getCountryData = function (country) {
-	const request = new XMLHttpRequest();
-	request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-	request.send();
-
-	request.addEventListener("load", function () {
-		const [data] = JSON.parse(this.responseText);
-		console.log(data);
-
-		const html = `<article class="country">
+const renderCountry = function (data, classname = '') {
+	const html = `<article class="country ${classname}">
   <img class="country__img" src="${data.flags.png}" />
   <div class="country__data">
     <h3 cla ss="country__name">${data.name.common}</h3>
@@ -20,11 +12,59 @@ const getCountryData = function (country) {
   </div>
 </article>`;
 
-		countriesContainer.insertAdjacentHTML("afterbegin", html);
-		countriesContainer.style.opacity = 1;
-	});
+	countriesContainer.insertAdjacentHTML("beforeend", html);
+	countriesContainer.style.opacity = 1;
 };
 
-getCountryData("bharat");
-getCountryData("usa");
-getCountryData("portugal");
+const getCountryAndNeighbour = function (country) {
+	const request = new XMLHttpRequest();
+	request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+	request.send();
+
+	request.addEventListener("load", function () {
+		const [data] = JSON.parse(this.responseText);
+		console.log(data);
+		renderCountry(data);
+
+		const [neighbour] = data.borders;
+		if (!neighbour) return;
+		console.log(neighbour);
+
+		const request2 = new XMLHttpRequest();
+		request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+		request2.send();
+
+		request2.addEventListener("load", function () {
+			const data2 = JSON.parse(this.responseText);
+			console.log(data2);
+			renderCountry(data2, 'neighbour');
+		}); 
+	});
+};
+ 
+getCountryAndNeighbour("bharat");
+// getCountryAndNeighbour("usa");
+// getCountryAndNeighbour("portugal");
+
+
+setTimeout(() => {
+	console.log(`1 sec passed`);
+	setTimeout(() => {
+		console.log(`2 sec passed`);
+		setTimeout(() => {
+			console.log(`3 sec passed`);
+			setTimeout(() => {
+				console.log(`4 sec passed`);
+				setTimeout(() => {
+					console.log(`5 sec passed`);
+					setTimeout(() => {
+						console.log(`6 sec passed`);
+						setTimeout(() => {
+							console.log(`7 sec passed`);
+						}, 1000)
+					}, 1000)
+				}, 1000)
+			}, 1000)
+		}, 1000)
+	}, 1000)
+}, 1000)
