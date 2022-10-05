4ea1ba58c321d1ffaec45c595afc523ecb441817
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-05 23:06:33 +0530

    loadNPause usign async await

--- script.js
+++ script.js
@@ -171,11 +171,7 @@ GOOD LUCK 😀
 
 // Promisfying setTimeout()
 
-const wait = function (seconds) {
-	return new Promise((resolve, reject) => {
-		setTimeout(resolve, seconds * 1000);
-	});
-};
+
 
 // wait(2)
 // 	.then(() => {
@@ -350,3 +346,37 @@ const get3Countries = async (c1, c2, c3) => {
 };
 
 get3Countries("india", "portugal", "usa");
+const wait = function (seconds) {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, seconds * 1000);
+	});
+};
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await
+ (only the part where the promise is consumed). Compare the two versions, think about the big
+  differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools
+ Network tab.
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function 
+(call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' 
+function.
+GOOD LUCK 😀
+*/
+
+const loadNPause = async function(){
+	let currentImage = await createImage('./img-1.jpg')
+	await wait(2);
+	currentImage.style.display = 'none'
+	currentImage = await createImage('./img-2.jpg')
+	await wait(2);
+	currentImage.style.display = 'none'
+}
+
+loadNPause()
